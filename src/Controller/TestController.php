5b9694c08d47274ad0fbdef2ac6b9cd272e888ac
martinberlin <martin@luckycloud.de>
2024-07-08 20:48:52 +0200

    Test Collection items

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,7 +2,11 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use App\Entity\TodoList;
+use App\Form\Type\EmailType;
+use App\Form\Type\TodoListFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,5 +20,38 @@ class TestController extends AbstractController
         );*/
         return $this->render('test/test.html.twig', []);
 
+    }
+    #[Route('/test/form')]
+    public function form(): Response
+    {
+
+        return $this->render('test/form.html.twig', [
+            'isSuccessful' => false
+        ]);
+    }
+
+    #[Route('/test/todo')]
+    public function todoList(): Response
+    {
+        $myTodo = new TodoList();
+        $form = $this->createForm(TodoListFormType::class, $myTodo, [
+            'action' => $this->generateUrl('todoSave'),
+            'method' => 'POST'
+        ]);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            exit("Form is valid:SAVE");
+        }
+
+        return $this->render('test/todo.html.twig', [
+            'mytodoList' => $myTodo,
+            'myForm' => $form
+        ]);
+    }
+
+    #[Route('/test/todo/save', 'todoSave')]
+    public function todoListSave(Request $request): Response
+    {
+        exit("todoListSave");
     }
 }
